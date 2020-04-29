4188bd73f7c139cf4dcc4f7855ced54f853a1949
Janin Culhaoglu <janin.culhaoglu@gmail.com>
2020-04-29 19:34:44 +0200

    Prepared the UI of Submit ViewController

--- Project33/Controller/SubmitViewController.swift
+++ Project33/Controller/SubmitViewController.swift
@@ -13,8 +13,64 @@ class SubmitViewController: UIViewController {
     var genre: String!
     var comments: String!
 
+    var stackView: UIStackView!
+    var status: UILabel!
+    var activityIndicator: UIActivityIndicatorView!
+
     // MARK: - ViewController LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "You're all set!"
+        navigationItem.hidesBackButton = true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        doSubmission()
+    }
+
+    // MARK: - Methods
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor.gray
+
+        stackView = UIStackView()
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .fillEqually
+        stackView.alignment = .center
+        stackView.axis = .vertical
+        view.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+
+        status = UILabel()
+        status.translatesAutoresizingMaskIntoConstraints = false
+        status.text = "Submitting..."
+        status.textColor = .white
+        status.font = UIFont.preferredFont(forTextStyle: .title1)
+        status.numberOfLines = 0
+        status.textAlignment = .center
+
+        activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.startAnimating()
+
+        stackView.addArrangedSubview(status)
+        stackView.addArrangedSubview(activityIndicator)
+    }
+    
+    func doSubmission() {
+
+    }
+    
+    @objc func doneTapped() {
+        _ = navigationController?.popToRootViewController(animated: true) // _ is to silence an “unused result”
     }
 }
